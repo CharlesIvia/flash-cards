8f0cab2b92403baa91113d229f2f4b09b38ad7d6
Charles Ivia <iviacharles@gmail.com>
2020-06-23 12:54:35 +0300

    Logic to created cards

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ const cardsData = [
     answer: "thisIsAVariable",
   },
 ];
+
+//Create all cards
+
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+//Create a single card
+
+function createCard(data, index) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  if (index === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `
+    <div class="inner-card">
+    <div class ="inner-card-front">
+     <p> ${data.question} </p>
+     </div>
+     <div class="inner-card-back>
+     <p>
+     ${data.answer} </p>
+     </div>
+     </div>`;
+
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
+  //Add to DOM cards
+
+  cardsEl.push(card);
+
+  cardsContainer.appendChild(card);
+}
